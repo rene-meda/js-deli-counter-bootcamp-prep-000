8f38dd05c121f072554a839aead648fae72c73f3
Adolpho Noguera <adolph518@gmail.com>
2018-03-30 23:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function currentLine(array){
     else
       var s = "";
       for (var i=0; i < array.length; i++){
-        if (i === 3){
+        if (array[i] === array.length){
           s += i + 1 + '.'+ " " + array[i] ;
         }
         else
